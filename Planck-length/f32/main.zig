543f74a8a907f04f08f7eb87bf3a7f8205511e1c
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-29 19:34:17 +0200

    Planck-length/f32: added file

---
+++ Planck-length/f32/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const h: f32 = 6.62607015E-34;
+    const h_reduced: f32 = h / (2.0 * std.math.pi);
+
+    const G: f32 = 6.67430E-11;
+
+    const c: f32 = 299792458.0;
+
+    print("Reduced Planck constant (f32) = {}\n", .{h_reduced});
+    print("Gravitational constant (f32) = {}\n", .{G});
+    print("Speed of light (f32) = {}\n", .{c});
+
+    const lp: f32 = std.math.sqrt((h_reduced * G) / std.math.pow(f32, c, 3.0));
+
+    print("Planck length (f32) = {}\n", .{lp});
+}
