83e809e8d585b2c514843413a6188726d1d4233c
Alexander Elfimow <my.free.mail@gmx.de>
2024-09-30 19:34:10 +0200

    Planck-length/f80: added

---
+++ Planck-length/f80/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const h: f80 = 6.62607015E-34;
+    const h_reduced: f80 = h / (2.0 * std.math.pi);
+
+    const G: f80 = 6.67430E-11;
+
+    const c: f80 = 299792458.0;
+
+    print("Reduced Planck constant (f80) = {}\n", .{h_reduced});
+    print("Gravitational constant (f80) = {}\n", .{G});
+    print("Speed of light (f80) = {}\n", .{c});
+
+    const lp: f80 = std.math.sqrt((h_reduced * G) / std.math.pow(f64, c, 3.0));
+
+    print("Planck length (f80) = {}\n", .{lp});
+}
