c54ee6f6977d8e15832c1cd96c97078915c03547
Alexander Elfimow <my.free.mail@gmx.de>
2024-10-04 20:07:02 +0200

    Planck-mass/f16: added

---
+++ Planck-mass/f16/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const h: f16 = 6.62607015E-34;
+    const h_reduced: f16 = h / (2.0 * std.math.pi);
+
+    const G: f16 = 6.67430E-11;
+
+    const c: f16 = 299792458.0;
+
+    print("Reduced Planck constant (f16) = {}\n", .{h_reduced});
+    print("Gravitational constant (f16) = {}\n", .{G});
+    print("Speed of light (f16) = {}\n", .{c});
+
+    const mp: f16 = std.math.sqrt((h_reduced * c) / G);
+
+    print("Planck mass (f16) = {}\n", .{mp});
+}
