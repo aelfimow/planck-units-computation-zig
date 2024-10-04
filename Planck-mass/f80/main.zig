f767c56cc941193b358d612fc1d3ce847f05b64a
Alexander Elfimow <my.free.mail@gmx.de>
2024-10-04 20:13:56 +0200

    Planck-mass/f80: added

---
+++ Planck-mass/f80/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    const h: f80 = 6.62607015E-34;
+    const h_reduced: f80 = h / (2.0 * std.math.pi);
+
+    const G: f80 = 6.67430E-11;
+
+    const c: f80 = 299792458.0;
+
+    print("Reduced Planck constant (f80) = {}\n", .{h_reduced});
+    print("Gravitational constant (f80) = {}\n", .{G});
+    print("Speed of light (f80) = {}\n", .{c});
+
+    const mp: f80 = std.math.sqrt((h_reduced * c) / G);
+
+    print("Planck mass (f80) = {}\n", .{mp});
+}
